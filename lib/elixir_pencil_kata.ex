aa6588596283aa1ef84967612b1733e0fca3e107
Jessie Morris <jmorris2@pillartechnology.com>
2019-01-26 15:49:59 -0500

    Initial simple tests/code

---
+++ lib/elixir_pencil_kata.ex
@@ -0,0 +1,15 @@
+defmodule ElixirPencilKata do
+  @moduledoc """
+  Documentation for ElixirPencilKata.
+  """
+
+  def write(text) do
+    text
+  end
+
+  def write(paper, text) do
+    paper <> text
+  end
+
+  
+end
