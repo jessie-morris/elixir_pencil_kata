bc75a715e2fc3b57346adab2bdcf212a68970c76
Jessie Morris <jmorris2@pillartechnology.com>
2019-01-26 16:48:37 -0500

    refactored to actor model, still not making sure durability is tested

--- lib/pencil.ex
+++ lib/pencil.ex
@@ -1,13 +1,17 @@
 defmodule Pencil do
-    def new(durability) do
+    def new(durability \\ 10) do
         {:ok, pencil_actor} = Agent.start_link(fn -> %{:durability => durability} end)
         pencil_actor
     end
 
-    def write(text) do
+    def write(pencil, text) do
+        text_size = String.length(text) 
+        Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> dura - text_size end) end)
         text
     end
-    def write(paper, text) do
+    def write(pencil, paper, text) do
+        text_size = String.length(text) 
+        Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> dura - text_size end) end) 
         paper <> text
     end
 end
--- test/pencil_test.exs
+++ test/pencil_test.exs
@@ -2,11 +2,13 @@ defmodule PencilTest do
   use ExUnit.Case
 
   test "Pencil writes to an empty paper" do
-    assert Pencil.write("hello") == "hello"
+    pencil = Pencil.new(10)
+    assert Pencil.write(pencil, "hello") == "hello"
   end
 
   test "Pencil appends to an existing piece of paper" do
+    pencil = Pencil.new(10)
     paper = "hello"
-    assert Pencil.write(paper, " world") == "hello world"
+    assert Pencil.write(pencil, paper, " world") == "hello world"
   end
 end
