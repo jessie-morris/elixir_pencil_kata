5362b61405eb680900ddf8d062f1c90beb333882
Jessie Morris <jmorris2@pillartechnology.com>
2019-01-26 16:52:08 -0500

    beginning to cook with gas

--- lib/pencil.ex
+++ lib/pencil.ex
@@ -4,11 +4,6 @@ defmodule Pencil do
         pencil_actor
     end
 
-    def write(pencil, text) do
-        text_size = String.length(text) 
-        Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> dura - text_size end) end)
-        text
-    end
     def write(pencil, paper, text) do
         text_size = String.length(text) 
         Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> dura - text_size end) end) 
--- test/pencil_test.exs
+++ test/pencil_test.exs
@@ -3,12 +3,18 @@ defmodule PencilTest do
 
   test "Pencil writes to an empty paper" do
     pencil = Pencil.new(10)
-    assert Pencil.write(pencil, "hello") == "hello"
+    assert Pencil.write(pencil, "", "hello") == "hello"
   end
 
   test "Pencil appends to an existing piece of paper" do
     pencil = Pencil.new(10)
     paper = "hello"
     assert Pencil.write(pencil, paper, " world") == "hello world"
+  end
+
+  test "Pencil durability drops 1 for each lowercase character" do
+    pencil = Pencil.new(10)
+    Pencil.write(pencil, "", "hello")
+    assert Agent.get(pencil, fn state -> state end) == %{:durability => 5}
   end
 end
