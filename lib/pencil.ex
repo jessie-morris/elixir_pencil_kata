f57a30543e269ca6b27d025e3c8cebebd2b5a9be
Jessie Morris <jmorris2@pillartechnology.com>
2019-01-27 16:54:11 -0500

    refactoring away text cost calculation methos

--- lib/pencil.ex
+++ lib/pencil.ex
@@ -5,22 +5,8 @@ defmodule Pencil do
     end
 
     def write(pencil, paper, text) do
-        durability_cost = text_cost(text) 
+        durability_cost = TextCost.text_cost(text) 
         Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> dura - durability_cost end) end) 
         paper <> text
-    end
-    def text_cost(text) do
-        String.to_charlist(text)
-        |> Enum.reduce(0, fn char, acc -> acc + char_cost(char) end )
-    end
-
-    def char_cost(char) when char in ?a..?z do
-        1
-    end
-    def char_cost(char) when char in ?A..?Z do
-        2
-    end
-    def char_cost(char) do
-        0
     end
 end
---
+++ lib/text_cost.ex
@@ -0,0 +1,19 @@
+defmodule TextCost do
+
+    def text_cost(text) do
+        String.to_charlist(text)
+        |> Enum.reduce(0, fn char, acc -> acc + char_cost(char) end )
+    end
+
+    defp char_cost(char) when char in ?a..?z do
+        1
+    end
+
+    defp char_cost(char) when char in ?A..?Z do
+        2
+    end
+
+    defp char_cost(char) do
+        0
+    end
+end
