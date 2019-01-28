2680de3f342be3bba055ceca94588be0ad6658c7
Jessie Morris <jmorris2@pillartechnology.com>
2019-01-27 21:48:03 -0500

    I think this refactor is an improvement, but still failing test

--- lib/pencil.ex
+++ lib/pencil.ex
@@ -5,8 +5,27 @@ defmodule Pencil do
     end
 
     def write(pencil, paper, text) do
-        durability_cost = TextCost.text_cost(text) 
-        Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> dura - durability_cost end) end) 
-        paper <> text
+        durability_cost = TextCost.text_cost(text)
+        pencil_durability = Agent.get(pencil, fn state -> Map.get(state, :durability) end) 
+        new_durability = max(0, pencil_durability - durability_cost) 
+
+        output_text = 
+            String.graphemes(text)
+            |> Enum.reduce(paper, fn char, acc -> write_char(char, pencil, acc) end)
+        
+        Agent.update(pencil, fn state -> Map.update!(state, :durability, fn dura -> new_durability end) end) 
+        output_text
+
+    end
+    def write_char(char, pencil, paper) do
+        IO.puts(char)
+        durability = Agent.get(pencil, fn state -> Map.get(state, :durability) end)
+        char_cost = TextCost.text_cost(char)
+        IO.puts(durability)
+        IO.puts(char_cost)
+        cond do
+            durability >= char_cost -> paper <> char
+            durability < char_cost -> paper <> " "
+        end
     end
 end
